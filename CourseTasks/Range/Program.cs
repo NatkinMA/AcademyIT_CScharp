bd13a83c5d5c06f57992caf3bd9ab223ce37150f
michael <michael@MONOBLOCK>
2017-02-04 20:50:01 +0700

    Создан класс Range. Объявлены два вещественных поля from, to. Описан контруктор, при помощи которого заполняются поля from, to. Функция для вычисления длины интервала. Создан метод IsInside, который принимает вещественное число и возвращает bool - результат проверки того, принадлежит ли число диапазону.

--- CourseTasks/Range/Program.cs
+++ CourseTasks/Range/Program.cs
@@ -11,6 +11,32 @@ namespace Range
         static void Main(string[] args)
         {
             Console.WriteLine("Программа для работы с классом Range");
+
+            Range objRange = new Range(-5.5, 5.5);
+            double dblNumber01 = 1.5;
+            double dblNumber02 = 6.5;
+
+            Console.WriteLine("Длина интервала ({0}, {1}): {2}.", objRange.From, objRange.To, objRange.GetInterval());
+
+            if (objRange.IsInside(dblNumber01))
+            {
+                Console.WriteLine("Число {0} входит в интервал ({1}, {2}).", dblNumber01, objRange.From, objRange.To);
+            }
+            else
+            {
+                Console.WriteLine("Число {0} не входит в интервал ({1}, {2}).", dblNumber01, objRange.From, objRange.To);
+            }
+
+            if (objRange.IsInside(dblNumber02))
+            {
+                Console.WriteLine("Число {0} входит в интервал ({1}, {2}).", dblNumber02, objRange.From, objRange.To);
+            }
+            else
+            {
+                Console.WriteLine("Число {0} не входит в интервал ({1}, {2}).", dblNumber02, objRange.From, objRange.To);
+            }
+
+            Console.WriteLine("Для выходя нажмите клавишу Утеук...");
             Console.ReadLine();
         }
     }
--- CourseTasks/Range/Range.cs
+++ CourseTasks/Range/Range.cs
@@ -8,5 +8,45 @@ namespace Range
 {
     class Range
     {
+        // Поля класса Range
+        private double dblFrom;
+        private double dblTo;
+
+        // Конструкторы класса Range
+        public Range(double from, double to)
+        {
+            this.dblFrom = from;
+            this.dblTo = to;
+        }
+
+        public Range()
+        { }
+
+        // Свойства класса Range
+        public double From
+        {
+            get { return dblFrom; }
+            set { dblFrom = value; }
+        }
+
+        public double To
+        {
+            get { return dblTo; }
+            set { dblTo = value; }
+        }
+
+        // Методы класса Range
+        public double GetInterval()
+        {
+            return Math.Abs(dblTo - dblFrom);
+        }
+
+        public bool IsInside(double number)
+        {
+            if ((number >= dblFrom)&&(number <= dblTo))
+            { return true; }
+            else
+            { return false; }
+        }
     }
 }
