662f902b793afa0a4ce15837d1e365ae5cc5493f
michael <michael@MONOBLOCK>
2017-02-04 15:58:08 +0700

    Проект Range

---
+++ CourseTasks/Range/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа для работы с классом Range");
+            Console.ReadLine();
+        }
+    }
+}
