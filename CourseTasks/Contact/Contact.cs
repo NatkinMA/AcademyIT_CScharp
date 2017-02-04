2a9191f767d09572e0057b2f8b72b260074d88d0
michael <michael@MONOBLOCK>
2017-02-04 16:30:01 +0700

    Демонстрация работы класса Contact

--- CourseTasks/Contact/Contact.cs
+++ CourseTasks/Contact/Contact.cs
@@ -8,6 +8,39 @@ namespace Contact
 {
     class Contact
     {
+        // Поля класса Contact
+        private string strName;
+        private string strSurname;
+        private string strPhoneNumber;
 
+        // Конструктор класса Contact
+        public Contact(string strName, string strSurname, string strPhoneNumber)
+        {
+            this.strName = strName;
+            this.strSurname = strSurname;
+            this.strPhoneNumber = strPhoneNumber;
+        }
+
+        public Contact()
+        { }
+
+        // Геттеры и сеттеры класса Contact
+        public string Name
+        {
+            get { return strName; }
+            set { strName = value; }
+        }
+
+        public string Surname
+        {
+            get { return strSurname; }
+            set { strSurname = value; }
+        }
+
+        public string PhoneNumber
+        {
+            get { return strPhoneNumber; }
+            set { strPhoneNumber = value; }
+        }
     }
 }
--- CourseTasks/Contact/Program.cs
+++ CourseTasks/Contact/Program.cs
@@ -11,6 +11,19 @@ namespace Contact
         static void Main(string[] args)
         {
             Console.WriteLine("Программа демонстрирующая работу с классом Contact");
+            // Console.ReadLine();
+
+            Contact objContact01 = new Contact("Иван", "Иванов", "+79991234567");
+            
+            Contact objContact02 = new Contact();
+            objContact02.Name = "Петр";
+            objContact02.Surname = "Петров";
+            objContact02.PhoneNumber = "+79992345678";
+
+            Console.WriteLine("Контакт № 1: {0} {1}, тел.: {2}", objContact01.Name, objContact01.Surname, objContact01.PhoneNumber);
+            Console.WriteLine("Контакт № 2: {0} {1}, тел.: {2}", objContact02.Name, objContact02.Surname, objContact02.PhoneNumber);
+
+            Console.WriteLine("Для завершения работы программы нажмите клавишу Утеук...");
             Console.ReadLine();
         }
     }
