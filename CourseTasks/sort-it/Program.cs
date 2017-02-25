e47143946e68302b0c8748087333e1f8f4b00a15
michael <michael@MONOBLOCK>
2017-02-25 07:01:55 +0700

    Доработал задачу ЦФТ.

--- CourseTasks/sort-it/Program.cs
+++ CourseTasks/sort-it/Program.cs
@@ -48,51 +48,68 @@ namespace sort_it
 
         private static void Help()
         {
-            Console.WriteLine("Справка о программе.");
+            Console.WriteLine("\nSort-it.exe {inputfilename} {outputfilename} {-i|-s} {-a|-b}");
+            Console.WriteLine("Сортировка целых чисел или строк из файла методом вставок. Входной файл должен содержать не более 100 строк.\n");
+            Console.WriteLine("- infilename\tОбязательный параметр. Имя файла с данными для обработки.");
+            Console.WriteLine("- outfilename\tОбязательный параметр. Имя файла с обработанными данными.");
+            Console.WriteLine("- -i|-s\t\tОбязательный параметр. Тип сортируемы данных:\n\t\t-i - целые числа;\n\t\t-s - строки.");
+            Console.WriteLine("- -a|-b\t\tОбязательный параметр. Направление сортировки данных:\n\t\t-a - по возрастанию;\n\t\t-d - по убыванию.");
         }
 
         static void Main(string[] args)
         {
-            if (args[0] == "-help")
+            if (args.Length == 0 || args[0].Equals("-help"))
             {
                 Help();
                 return;
             }
+
             if (args.Length != 4)
             {
-                Console.WriteLine();
+                Console.WriteLine("Неверное количество параметров для запуска.");
+                Help();
+                return;
             }
-            
+
             Console.WriteLine("Чтение данных из файла: {0}", args[0]);
 
+            List<string> ListStringOfFile = new List<string>();
 
-
-            object[] array = new object[File.ReadAllLines(args[0]).Length];
-            using (StreamReader reader = new StreamReader(args[0]))
+            try
             {
-                int index = 0;
-                string currentLine;
-                while ((currentLine = reader.ReadLine()) != null)
+                using (StreamReader reader = new StreamReader(args[0]))
                 {
-                    array[index] = currentLine;
-                    index++;
+                    string str;
+                    while ((str = reader.ReadLine()) != null)
+                    {
+                        ListStringOfFile.Add(str);
+                    }
                 }
             }
-
-            Console.WriteLine("Сортировка данных методом вставок (insertion sort)");
-            array = InsertionSort(array, args[2], args[3]);
-
-            Console.WriteLine("Запись данных в файл: {0}", args[1]);
-            using (StreamWriter writer = new StreamWriter(args[1]))
+            catch (Exception e)
             {
-                foreach(object line in array)
-                {
-                    writer.WriteLine(line.ToString());
-                }
+                Console.WriteLine("Не удалось прочитать файл:");
+                Console.WriteLine(e.Message);
             }
 
-            Console.WriteLine("Для выхода из программы нажмите Утеук...");
-            Console.ReadLine();
+            //Console.WriteLine("Сортировка данных методом вставок (insertion sort)");
+            //array = InsertionSort(array, args[2], args[3]);
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(args[1]))
+                {
+                    foreach (string str in ListStringOfFile)
+                    {
+                        writer.WriteLine(str);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Не удалось записать файл:");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
