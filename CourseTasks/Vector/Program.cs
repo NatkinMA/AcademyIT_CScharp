809a8fb9f3c573bac443f9e342aa45380adba8de
Michael Natkin <NatkinMA@ya.ru>
2017-09-26 22:28:01 +0700

    Реализован класс Vector для векторов вещественных чисел размерности n.

--- CourseTasks/Vector/Program.cs
+++ CourseTasks/Vector/Program.cs
@@ -10,7 +10,53 @@ namespace Vector
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!!!");
+            try
+            {
+                Vector vector1 = new Vector(10);
+                Console.WriteLine(vector1.ToString());
+                Console.WriteLine("Размер вектора: {0}.", vector1.Size);
+                Vector vector2 = new Vector(1.2, 1.3);
+                Console.WriteLine(vector2.ToString());
+                Console.WriteLine("Размер вектора: {0}.", vector2.Size);
+                Vector vector3 = new Vector(vector2);
+                Console.WriteLine(vector3.ToString());
+                Console.WriteLine("Размер вектора: {0}.", vector3.Size);
+                Vector vector4 = new Vector(5, 1.7, 1.9, 2.2);
+                Console.WriteLine(vector4.ToString());
+                Console.WriteLine("Размер вектора: {0}.", vector4.Size);
+                Console.WriteLine("Умножение вектора на скаляр k = 2,3.");
+                vector4.Multiplication(2.3);
+                Console.WriteLine(vector4.ToString());
+                Console.WriteLine("Размер вектора: {0}.", vector4.Size);
+                Console.WriteLine("Разворачиваем вектор.");
+                vector4.Deploy();
+                Console.WriteLine(vector4.ToString());
+                Console.WriteLine("Размер вектора: {0}.", vector4.Size);
+                Console.WriteLine("Длина вектора: {0}.", vector4.Length);
+                Console.WriteLine("Значение компоненты вектора с индексом 1: {0}", vector4.GetDimention(1));
+                vector4.SetDimention(4, 6.7);
+                Console.WriteLine("Устанавливаем компонент вектора с индексом 4: {0}", vector4.GetDimention(4));
+                Console.WriteLine("Складываем два вектора ({0}) и ({1}) с помощью статического метода.", vector2.ToString(), vector4.ToString());
+                Vector vector5 = Vector.Addition(vector2, vector4);
+                Console.WriteLine("Суммарный вектор: {0}", vector5.ToString());
+                Console.WriteLine("Прибавляем к вектору ({0}) вектор ({1}).", vector2.ToString(), vector4.ToString());
+                vector2.Add(vector4);
+                Console.WriteLine("Получившийся вектор: ({0})", vector2.ToString());
+                Console.WriteLine("Вычитаем из вектора ({0}) вектор ({1}).", vector2.ToString(), vector4.ToString());
+                vector2.Subtract(vector4);
+                Console.WriteLine("Получившийся вектор: ({0})", vector2.ToString());
+                Console.WriteLine("Вычитаем c помощью статического метода из вектора ({0}) вектор ({1}).", vector2.ToString(), vector4.ToString());
+                Vector vector6 = Vector.ScalarProduct(vector2, vector4);
+                Console.WriteLine("Получившийся вектор: ({0})", vector6.ToString());
+                Console.WriteLine("Сравнение вектора ({0}) и вектора ({1}): {2}.", vector2.ToString(), vector4.ToString(), vector2.Equals(vector4).ToString());
+                Console.WriteLine("Сравнение вектора ({0}) и вектора ({1}): {2}.", vector2.ToString(), vector2.ToString(), vector2.Equals(vector2).ToString());
+                Console.WriteLine("Хэш-код вектора ({0}) = ({1}).", vector2.ToString(), vector2.GetHashCode());
+            }
+            catch (ArgumentOutOfRangeException exception)
+            {
+                Console.WriteLine(exception);
+            }
+            Console.WriteLine("Для выхода нажмите Утеук...");
             Console.ReadLine();
         }
     }
--- CourseTasks/Vector/Vector.cs
+++ CourseTasks/Vector/Vector.cs
@@ -1,14 +1,210 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Vector
 {
     class Vector
     {
-        private double [] dimentions;
+        private double[] dimentions;
 
+        public Vector(int n)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Некорректное значение размера массива.");
+            }
+            dimentions = new double[n];
+        }
+
+        public Vector(Vector vector)
+        {
+            dimentions = (double[]) vector.dimentions.Clone();
+        }
+
+        public Vector(params double[] dimentions)
+        {
+            this.dimentions = (double[]) dimentions.Clone();
+        }
+
+        public Vector(int n, params double[] dimentions)
+        {
+            if (n <= 0) throw new ArgumentOutOfRangeException("Некорректное значение размера массива.");
+            this.dimentions = new double[n];
+            dimentions.CopyTo(this.dimentions, 0);
+        }
+
+        public override string ToString()
+        {
+            return String.Join(", ", dimentions);
+        }
+
+        public int Size => dimentions.Length;
+
+        public void Multiplication (double k)
+        {
+            for (int i = 0; i < dimentions.Length; i++) dimentions[i] = k * dimentions[i];
+        }
+
+        public void Deploy()
+        {
+            for (int i = 0; i < dimentions.Length; i++) dimentions[i] = -1 * dimentions[i];
+        }
+
+        public double Length
+        {
+            get
+            {
+                double length = 0;
+                foreach (double dimention in dimentions) length = length + Math.Pow(dimention, 2);
+                return Math.Sqrt(length);
+            }
+        }
+
+        public void SetDimention(int i, double dimention) { dimentions[i] = dimention; }
+
+        public double GetDimention(int i) { return dimentions[i]; }
+
+        public void Add(Vector vector)
+        {
+            if (this.Size < vector.Size)
+            {
+                double[] temp = (double[])dimentions.Clone();
+                this.dimentions = new double[vector.Size];
+                temp.CopyTo(this.dimentions, 0);
+            }
+            for (int i = 0; i < vector.Size; i++)
+            {
+                this.SetDimention(i, this.GetDimention(i) + vector.GetDimention(i));
+            }
+        }
+
+        public void Subtract(Vector vector)
+        {
+            if (this.Size < vector.Size)
+            {
+                double[] temp = (double[])dimentions.Clone();
+                this.dimentions = new double[vector.Size];
+                temp.CopyTo(this.dimentions, 0);
+            }
+            for (int i = 0; i < vector.Size; i++)
+            {
+                this.SetDimention(i, this.GetDimention(i) - vector.GetDimention(i));
+            }
+            /*
+            vector.Deploy(); // так делать нельзя, потому что меняется объект.
+            this.Add(vector);
+            */
+        }
+
+        public static Vector Addition(Vector vector1, Vector vector2)
+        {
+            int size = 0;
+
+            if (vector1.Size >= vector2.Size)
+            {
+                size = vector1.Size;
+                vector2 = new Vector(size, vector2.dimentions);
+            } else {
+                size = vector2.Size;
+                vector1 = new Vector(size, vector1.dimentions);
+            }
+
+            Vector vector = new Vector(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                vector.SetDimention(i, vector1.GetDimention(i) + vector2.GetDimention(i));
+            }
+
+            return vector;
+        }
+
+        public static Vector Subtraction(Vector vector1, Vector vector2)
+        {
+            int size = 0;
+
+            if (vector1.Size >= vector2.Size)
+            {
+                size = vector1.Size;
+                vector2 = new Vector(size, vector2.dimentions);
+            }
+            else
+            {
+                size = vector2.Size;
+                vector1 = new Vector(size, vector1.dimentions);
+            }
+
+            Vector vector = new Vector(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                vector.SetDimention(i, vector1.GetDimention(i) - vector2.GetDimention(i));
+            }
+
+            return vector;
+            /*
+            vector2.Deploy();
+            return Addition(vector1, vector2);
+            */
+        }
+
+        public static Vector ScalarProduct(Vector vector1, Vector vector2)
+        {
+            int size = 0;
+
+            if (vector1.Size >= vector2.Size)
+            {
+                size = vector1.Size;
+                vector2 = new Vector(size, vector2.dimentions);
+            }
+            else
+            {
+                size = vector2.Size;
+                vector1 = new Vector(size, vector1.dimentions);
+            }
+
+            Vector vector = new Vector(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                vector.SetDimention(i, vector1.GetDimention(i) * vector2.GetDimention(i));
+            }
+
+            return vector;
+        }
+
+        public bool Equals(Vector vector)
+        {
+            if (vector == null)
+            {
+                return false;
+            }
+            if (vector == this)
+            {
+                return true;
+            }
+            if (this.Length == vector.Length)
+            {
+                for(int i = 0; i < vector.Length; i++)
+                {
+                    if (!this.GetDimention(i).Equals(vector.GetDimention(i))) return false;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || !(obj is Vector))
+            {
+                return false;
+            }
+            return this.Equals((Vector)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
     }
 }
